d2bb4f9efcfe4acd10d56070cf6e82ebbbc23f8f
Richard Carter <richard.carter@firma-engineering.com>
2025-07-16 10:49:27 +0100

    function change

--- helloWorld.py
+++ helloWorld.py
@@ -1,12 +1,10 @@
-# def main():
-#     print(greet)
+def main():
+    print(greet)
 
 
-# def greet():
-#     greeting = "Hello World"
-#     return greeting
+def greet():
+    greeting = "Hello World"
+    return greeting
 
 
-# if __name__ == "__main__":
-#     main()
-print("Hello World")
+main()
